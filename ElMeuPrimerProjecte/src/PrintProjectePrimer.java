46c798a31672011e4439fab46e8aa77121473a5e
Giuliano Aurora <127998536+Kaki9hd@users.noreply.github.com>
2025-01-30 16:11:29 +0100

    Add files via upload

---
+++ ElMeuPrimerProjecte/src/PrintProjectePrimer.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author epozo
+ *
+ */
+public class PrintProjectePrimer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("El meu primer projecte JAVA");
+	}
+
+}
